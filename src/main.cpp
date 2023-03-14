8f1175013d3f5c91a9d03ff067be93d6d18663b7
DanielFyy <danielfabry2000@gmail.com>
2023-03-14 19:14:01 +0200

    version

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,8 @@
 int main(int argc, char const *argv[])
 {   
 
-    
-    //scene is heap instantiated for debubgging 
-    //for the destructor to be called before displaying created and destroyed objects
-    Scene* scene = new Scene;
-    scene->main_menu();
-    delete scene;
-    
-    std::cout << "Created objects: " << created << "\n";
-    std::cout << "Destroyed objects: " << destroyed << "\n";
+    Scene scene;
+    scene.main_menu();
 
     return 0;
 }
